2f5586ec22721ecc218b5130ac5cba04ee72e8b5
Алексей Тяпкин <a.e.tyapkin@gmail.com>
2023-04-09 02:16:45 +0300

    Удаление лишних импортов

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import "./App.scss";
-import { Header } from "./components/layout/header/Header";
 import { Footer } from "./components/layout/footer/Footer";
-import { routes } from "./common/routes";
-import {
-  BrowserRouter,
-  Outlet,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { Header } from "./components/layout/header/Header";
 
 function App() {
+
   return (
     <div className="App">
-      <Header />
-      <article>
-        <Outlet />
-      </article>
-      <Footer />
+        <Header />
+        <article>
+          <Outlet />
+        </article>
+        <Footer />
     </div>
   );
 }
